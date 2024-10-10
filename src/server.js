79d7e56c8239da629312cd1a5e4e3550a816ba09
coder nelly <codernelly34@gmail.com>
2024-10-10 19:29:14 +0100

    testing format on commit

--- src/server.js
+++ src/server.js
@@ -1,11 +1,13 @@
-import "dotenv/config"
+import "dotenv/config";
 import { app } from "./app.js";
 import { connectDB } from "./utils/connectDB.js";
 
-const PORT = process.env.PORT || 3031
+const PORT = process.env.PORT || 3031;
 
-connectDB().then(()=>{
-  app.listen(PORT,()=>console.log(`Server is running on port ${PORT}`))
-}).catch((error)=>{
-  console.log(`MongoDB connection error`);
-})
+connectDB()
+  .then(() => {
+    app.listen(PORT, () => console.log(`Server is running on port ${PORT}`));
+  })
+  .catch((error) => {
+    console.log(`MongoDB connection error`);
+  });
