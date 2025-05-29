ee8b28a505f07b31f2ff729118b56dfcd60d14fb
Coder Nelly <codernelly34@gmail.com>
2025-05-29 21:48:41 +0100

    feat: add login route to local authentication router

--- src/routes/localAuth.routes.js
+++ src/routes/localAuth.routes.js
@@ -1,11 +1,13 @@
 import { Router } from "express";
 import verifyRequestBody from "../middlewares/verifyRequestBody.js";
 import registerAccount from "../controllers/localAuth/registerUser.js";
+import loginAccount from "../controllers/localAuth/loginUser.js";
 
 const localAuthRouter = Router();
 
 localAuthRouter.use(verifyRequestBody);
 
 localAuthRouter.route("/register").post(registerAccount);
+localAuthRouter.route("/login").post(loginAccount);
 
 export default localAuthRouter;
