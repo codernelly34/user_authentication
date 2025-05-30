b12e3539de599f99751c12ceb179f3e8f6fe54b7
Coder Nelly <codernelly34@gmail.com>
2025-05-30 08:31:28 +0100

    feat: add getUserInfo controller to retrieve user details by email when they are login

---
+++ src/controllers/localAuth/getUserInfo.js
@@ -0,0 +1,26 @@
+import expressAsyncHandler from "express-async-handler";
+import userModel from "../../models/user.model.js";
+
+const getUserInfo = expressAsyncHandler(async (req, res) => {
+  const email = req.user;
+
+  const user = await userModel.findOne({ email });
+
+  if (!user) {
+    throw new AppError(
+      "Unable to get User info, Please try again later",
+      500,
+      "ServerError"
+    );
+  }
+
+  const resBody = {
+    firstName: user.firstName,
+    lastName: user.lastName,
+    email: user.email,
+  };
+
+  res.status(200).json(resBody);
+});
+
+export default getUserInfo;
