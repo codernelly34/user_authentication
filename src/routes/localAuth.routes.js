de7ccc50a2590d1c0b7a4d7118d348a3a7a30cb7
Coder Nelly <codernelly34@gmail.com>
2025-06-02 10:27:17 +0100

    feat: add logout and refresh-access routes to localAuthRouter

--- src/routes/localAuth.routes.js
+++ src/routes/localAuth.routes.js
@@ -2,6 +2,8 @@ import { Router } from "express";
 import verifyRequestBody from "../middlewares/verifyRequestBody.js";
 import registerAccount from "../controllers/localAuth/registerUser.js";
 import loginAccount from "../controllers/localAuth/loginUser.js";
+import signOut from "../controllers/localAuth/logoutUser.js";
+import refreshAccess from "../controllers/localAuth/refreshAccess.js";
 
 const localAuthRouter = Router();
 
@@ -9,5 +11,7 @@ localAuthRouter.use(verifyRequestBody);
 
 localAuthRouter.route("/register").post(registerAccount);
 localAuthRouter.route("/login").post(loginAccount);
+localAuthRouter.get("/logout", signOut);
+localAuthRouter.get("/refresh-access", refreshAccess);
 
 export default localAuthRouter;
