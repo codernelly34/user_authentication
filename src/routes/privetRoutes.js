243cc518a4182955bbbee9a3d9de2870ea26c778
Coder Nelly <codernelly34@gmail.com>
2025-06-02 21:08:26 +0100

    feat: add request body verification to update user info route

--- src/routes/privetRoutes.js
+++ src/routes/privetRoutes.js
@@ -2,12 +2,13 @@ import { Router } from "express";
 import verifyAccess from "../middlewares/verifyAccess.js";
 import getUserInfo from "../controllers/localAuth/getUserInfo.js";
 import updateUserInfo from "../controllers/localAuth/updateUserInfo.js";
+import verifyRequestBody from "../middlewares/verifyRequestBody.js";
 
 const privetRoutes = Router();
 
 privetRoutes.use(verifyAccess);
 
 privetRoutes.get("/get-user-info", getUserInfo);
-privetRoutes.put("/update-user-info", updateUserInfo);
+privetRoutes.put("/update-user-info", verifyRequestBody, updateUserInfo);
 
 export default privetRoutes;
