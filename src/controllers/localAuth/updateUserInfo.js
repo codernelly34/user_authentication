8e495d22d01f54b49853d707678a8a0b62b53f7f
Coder Nelly <codernelly34@gmail.com>
2025-06-02 06:14:21 +0100

    feat: add updateUserInfo controller and route for updating user information

---
+++ src/controllers/localAuth/updateUserInfo.js
@@ -0,0 +1,37 @@
+import expressAsyncHandler from "express-async-handler";
+import userModel from "../../models/user.model.js";
+import AppError from "../../utils/AppError.js";
+
+const updateUserInfo = expressAsyncHandler(async (req, res) => {
+  const email = req.user;
+  const updateData = req.body;
+
+  // Check if request body is empty
+  if (Object.keys(updateData).length === 0) {
+    throw new AppError(
+      "No update data provided. Please include at least one field to update.",
+      400
+    );
+  }
+
+  try {
+    const updatedUser = await userModel.findOneAndUpdate(
+      email,
+      { $set: updateData },
+      { new: true, runValidators: true }
+    );
+
+    if (!updatedUser) {
+      return res.status(404).json({ message: "User not found" });
+    }
+
+    res.status(200).json({
+      message: "User info updated successfully",
+      user: updatedUser,
+    });
+  } catch (error) {
+    throw new AppError("Internal server error", 500, "ServerError", error);
+  }
+});
+
+export default updateUserInfo;
--- src/routes/privetRoutes.js
+++ src/routes/privetRoutes.js
@@ -1,11 +1,13 @@
 import { Router } from "express";
 import verifyAccess from "../middlewares/verifyAccess.js";
 import getUserInfo from "../controllers/localAuth/getUserInfo.js";
+import updateUserInfo from "../controllers/localAuth/updateUserInfo.js";
 
 const privetRoutes = Router();
 
 privetRoutes.use(verifyAccess);
 
 privetRoutes.get("/get-user-info", getUserInfo);
+privetRoutes.put("/update-user-info", updateUserInfo);
 
 export default privetRoutes;
