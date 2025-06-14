43a224cef67ea2344688771984e5c466b957b26b
Coder Nelly <codernelly34@gmail.com>
2025-06-14 10:58:07 +0100

    refactor: streamline token handling in refreshAccess controller

--- src/controllers/refreshAccess.js
+++ src/controllers/refreshAccess.js
@@ -3,6 +3,7 @@ import userModel from "../models/user.model.js";
 import AppError from "../utils/AppError.js";
 import jwt from "jsonwebtoken";
 import ms from "ms";
+import issueAuthToken from "../utils/issueAuthToken.js";
 
 const refreshAccess = expressAsyncHandler(async (req, res) => {
   const { refreshToken } = req.cookies;
@@ -38,7 +39,7 @@ const refreshAccess = expressAsyncHandler(async (req, res) => {
   if (!userInDB) {
     try {
       const hackedUser = await userModel.findOne({
-        email: decoded.email,
+        id: decoded.id,
       });
       if (hackedUser) {
         hackedUser.token = "";
@@ -57,46 +58,7 @@ const refreshAccess = expressAsyncHandler(async (req, res) => {
     }
   }
 
-  // Create access and refresh token
-  const createAccessToken = jwt.sign(
-    { email: userInDB.email },
-    process.env.accessTokenKey,
-    {
-      expiresIn: ms("5m"),
-    }
-  );
-  const createRefreshToken = jwt.sign(
-    { email: userInDB.email },
-    process.env.refreshTokenKey,
-    {
-      expiresIn: ms("15m"),
-    }
-  );
-
-  // Save refresh token to user; throw error if save fails
-  try {
-    userInDB.token = createRefreshToken;
-    userInDB.save();
-  } catch (error) {
-    throw new AppError(
-      "We encounter and error while trying to log you into your account. please try again later",
-      500,
-      "ServerError",
-      error
-    );
-  }
-
-  // Set access and refresh tokens as secure, HTTP-only cookies with expiration times
-  res.cookie("accessToken", createAccessToken, {
-    maxAge: ms("5m"),
-    httpOnly: true,
-    secure: true,
-  });
-  res.cookie("refreshToken", createRefreshToken, {
-    maxAge: ms("15m"),
-    httpOnly: true,
-    secure: true,
-  });
+  await issueAuthToken(userInDB, res);
 
   res.status(200).json({ message: "Access refreshed" });
 });
